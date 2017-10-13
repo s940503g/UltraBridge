b081b3c4e77fd0a353951b9f383df4ceae0d5089
s940503g <s940503g@hotmail.com>
2017-10-13 19:35:31 +0800

    Auto detect new gateway.

--- test.js
+++ test.js
@@ -2,8 +2,11 @@
 const hap_nodejs = require('hap-nodejs');
 const Gateway = require('./lib/Gateway.js');
 const dgram = require('dgram');
+const debug = require('debug')('test');
 
-var port = 51826;
+var gatewayList = {};
+
+var port = 5050;
 let pincode = "222-21-266";
 
 var socket = dgram.createSocket('udp4');
@@ -14,26 +17,49 @@ socket.bind(function () {
 });
 
 var message = new Buffer('WHOIS_AVA_ZWAVE#');
+
 socket.send(message, 0, message.length, 10000, '255.255.255.255', function (err, bytes) {
 	if (err) console.log(err);
-//	socket.close();
 });
 
+setInterval(function () {
+	socket.send(message, 0, message.length, 10000, '255.255.255.255', function (err, bytes) {
+		if (err) console.log(err);
+	});
+}, 15000);
+
 server.on('message', function (msg, rinfo) {
-	msg = msg.toString('utf8');
-	console.log('receive message: ' + msg);
+	msg = msg.toString('utf8').split(/&/);
+	//console.log('Receive message: ' + msg);
 	
-	if (msg.match(/^RE_WHOIS_AVA_ZWAVE#/)) {
+	let title = msg[0];
+	let mac = msg[1];
+	let model = msg[2];
+
+	if (title.match(/^RE_WHOIS_AVA_ZWAVE#/)) {
 		hap_nodejs.init();
 	
 		let address = rinfo.address;
 
-		console.log('get message form'+address);
+		if (gatewayList[mac]) {
+			let old_gw = gatewayList[mac];
+			if (old_gw.setting.ip != address) {
+				old_gw.destroy();
+				let gateway = new Gateway('admin', '123456', address);
+				gateway.publish(pincode, port++);
+				gatewayList[mac] = gateway;
+				debug('Reset ' + mac);
+			}
+			debug(address + " is Alive.");	
+		}else{
+			let gateway = new Gateway('admin', '123456', address);
+			gateway.publish(pincode, port++);
+			gatewayList[mac] = gateway;
+			
+			debug('Create new Gateway at ip: ' + address);
+		}
 
-		let gateway = new Gateway('admin', '123456', address);
-
-		gateway.publish(pincode, port);
-	} else if (msg.match(/^WHOIS_AVA_BRIDGE#/)) {
+	} else if (title.match(/^WHOIS_AVA_BRIDGE#/)) {
 		
 		let message = new Buffer('RE_WHOIS_AVA_BRIDGE#');
 		socket.send(message, 0, message.length, 10000, '255.255.255.255', function (err, bytes) {
@@ -44,3 +70,5 @@ server.on('message', function (msg, rinfo) {
 });
 
 server.bind(10000);
+
+
