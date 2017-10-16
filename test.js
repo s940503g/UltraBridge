07534e63dcbf69337ed789aed02ac787cec7f325
s940503g <s940503g@hotmail.com>
2017-10-16 20:02:11 +0800

    minor change

--- test.js
+++ test.js
@@ -1,23 +1,4 @@
 'use strict';
-<<<<<<< HEAD
-const debug = require('debug')('Gateway');
-const events = require('events');
-const hap_nodejs = require('hap-nodejs');
-const Accessory = hap_nodejs.Accessory;
-const Service = hap_nodejs.Service;
-const Characteristic = hap_nodejs.Characteristic;
-const uuid = hap_nodejs.uuid;
-const child_process = require('child_process');
-const parser = require('xml2json').toJson;
-const Bridge = hap_nodejs.Bridge;
-const Gateway = require('./lib/Gateway.js').Gateway;
-var storage = require('node-persist');
-
-storage.initSync();
-
-let gw = new Gateway('admin', '123456', '192.168.1.101');
-gw.publish('222-21-266', 5001);
-=======
 const hap_nodejs = require('hap-nodejs');
 const Gateway = require('./lib/Gateway.js');
 const dgram = require('dgram');
@@ -45,19 +26,17 @@ setInterval(function () {
 	socket.send(message, 0, message.length, 10000, '255.255.255.255', function (err, bytes) {
 		if (err) console.log(err);
 	});
-}, 15000);
+}, 10000);
 
 server.on('message', function (msg, rinfo) {
 	msg = msg.toString('utf8').split(/&/);
-	//console.log('Receive message: ' + msg);
-	
 	let title = msg[0];
 	let mac = msg[1];
 	let model = msg[2];
 
 	if (title.match(/^RE_WHOIS_AVA_ZWAVE#/)) {
 		hap_nodejs.init();
-	
+
 		let address = rinfo.address;
 
 		if (gatewayList[mac]) {
@@ -69,26 +48,22 @@ server.on('message', function (msg, rinfo) {
 				gatewayList[mac] = gateway;
 				debug('Reset ' + mac);
 			}
-			debug(address + " is Alive.");	
+			debug(address + " is Alive.");
 		}else{
 			let gateway = new Gateway('admin', '123456', address);
 			gateway.publish(pincode, port++);
 			gatewayList[mac] = gateway;
-			
+
 			debug('Create new Gateway at ip: ' + address);
 		}
 
 	} else if (title.match(/^WHOIS_AVA_BRIDGE#/)) {
-		
+
 		let message = new Buffer('RE_WHOIS_AVA_BRIDGE#');
 		socket.send(message, 0, message.length, 10000, '255.255.255.255', function (err, bytes) {
 			if (err) console.log(err);
-//			socket.close();
 		});
 	}
 });
 
 server.bind(10000);
-
-
->>>>>>> b081b3c4e77fd0a353951b9f383df4ceae0d5089
