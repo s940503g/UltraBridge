2a86143e19867cd522f6fa1f14ad346290c8e384
s940503g <s940503g@hotmail.com>
2017-11-03 11:25:39 +0800

    add callback function for checking bridging success or not.

--- app.js
+++ app.js
@@ -38,7 +38,9 @@ app.get('/register', function (req, res) {
 		if (!info) throw `Can't find gateway ${mac}. Please check or rescan.`;
 		if (acc && pwd) {
 			gateway.destroy();
-			gateway.BridgeGateway(acc, pwd);
+			gateway.BridgeGateway(acc, pwd, (err) => {
+				if (err) throw err;
+			});
 		}
 		gateway.publish(GatewayManager.port++, GatewayManager.pincode);
 
