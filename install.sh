eb71a099eca642726ae8a21dfb417ea6db768946
s940503g <s940503g@hotmail.com>
2017-11-12 12:07:59 +0800

    add nodejs install script.

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 sudo apt-get update
+curl -sL https://deb.nodesource.com/setup_8.x
+sudo apt-get install nodejs -y
 sudo apt-get install git-core libnss-mdns libavahi-compat-libdnssd-dev -y
 npm install
