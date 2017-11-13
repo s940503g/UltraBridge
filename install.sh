ec71126b3455a6017e8c8e11651b55d2f089c522
s940503g <s940503g@hotmail.com>
2017-11-13 10:58:47 +0800

    use sudo when npm install module.

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ sudo apt-get update
 curl -sL https://deb.nodesource.com/setup_8.x
 sudo apt-get install nodejs -y
 sudo apt-get install git-core libnss-mdns libavahi-compat-libdnssd-dev -y
-npm install
+sudo npm install
